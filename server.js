e834625faad78ac0a73c629ef8698d802a9a973f
Vita Fomin <vita.bukhalo@gmail.com>
2023-05-04 18:33:18 -0400

    was able to debug and have my console.logs show in terminal

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const path = require("path");
-const { appendFile, readFile } = require("fs/promises");
+const fs = require("fs/promises");
+const { writeFile } = require("fs");
+// const noteData = require
 
 const PORT = process.env.PORT || 3001;
 
@@ -18,6 +20,58 @@ app.get("/", (req, res) =>
 app.get("/notes", (req, res) =>
   res.sendFile(path.join(__dirname, "/public/notes.html"))
 );
+
+
+app.get("/api/notes", (req, res) => {
+  console.log(`${req.method} request received for notes`);
+  fs.readFile("./db/db.json").then((notes) => res.json(JSON.parse(notes)))
+
+
+});
+
+app.post("/api/notes", (req, res) => {
+  console.log(`${req.method} request recieved to add a new note`);
+
+  console.log(req.body);
+
+  const { noteTitle, noteText } = req.body;
+
+  if (noteTitle && noteText) {
+    const newNote = {
+      noteTitle,
+      noteText
+    };
+
+    readAndAppend(newNote, "./db/db.json");
+    res.json("Added new note")
+  }
+  // else {
+  //   res.error("Error in adding a new note");
+  // }
+});
+
+  //   fs.readFile("./db/db.json", "utf8", (err,notes) => {
+  //     if (err) {
+  //       console.error(err);
+  //     }
+  //     else{
+  //       const parsedNotes = JSON.parse(notes);
+        
+  //       parsedNotes.push(newNote);
+
+  //       fs.writeFile("./db/db.json", JSON.stringify(parsedNotes), (writeErr) => writeErr ? console.error(writeErr) : console.log("Successfully updated notes"))
+  //     }
+  //   })
+
+  //   const response = {
+  //     body: newNote
+  //   };
+
+  //   res.status(200).json(response)
+  // }
+  // else {
+  //   res.status(500).json("Error to add this note")
+  // };
 
 
 
