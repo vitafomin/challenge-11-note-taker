d2f6551735ad407f7a16c3c1ad92df2c5853ce08
Vita Fomin <vita.bukhalo@gmail.com>
2023-05-06 19:57:59 -0400

    added a delete method to be able to delete notes, tested

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const path = require("path");
 const fs = require("fs/promises");
 const { writeFile } = require("fs");
-const { uuid } = require('uuidv4');
+const { uuid } = require("uuidv4");
 // const noteData = require
 
 const PORT = process.env.PORT || 3001;
@@ -44,70 +44,91 @@ app.post("/api/notes", (req, res) => {
     console.log("New Note: ", newNote);
     // });
 
-    /*
-    fs.readFile("./db/db.json", "utf8", (err, notes) => {
-      if (err) {
-        console.error(err);
-      } else {
-        const parsedNotes = JSON.parse(notes);
-        
-        parsedNotes.push(newNote)
-        .then (
-          fs.writeFile("./db/db.json", JSON.stringify(parsedNotes), (writeErr) =>
-          writeErr
-          ? console.error(writeErr)
-            : console.log("Successfully updated notes")
-            ));
-          }
-        });
-    */
-
-    // We are Grabbing or Retrieving our Saved Data/Dataset
-    fs.readFile('./db/db.json', 'utf8')
-      .then(notes => {
+    // Retrieving the Saved Data/Dataset
+    fs.readFile("./db/db.json", "utf8")
+      .then((notes) => {
         console.log("Data: ", notes);
         const parsedNotes = JSON.parse(notes);
         // We add/manipulate the original data
-        parsedNotes.push(newNote)
+        parsedNotes.push(newNote);
 
         // We have to write the NEW data
-        fs.writeFile('./db/db.json', JSON.stringify(parsedNotes))
+        fs.writeFile("./db/db.json", JSON.stringify(parsedNotes))
           .then(() => {
-            console.log("Data added successfully!");
-            res.status(201).json({ message: "New Note Added"});
+            console.log("Note added successfully!");
+            res.status(201).json({ message: "New Note Added" });
           })
-          .catch(err => {
+          .catch((err) => {
             console.log(err);
             res.status(500).json(err);
           });
       })
-      .catch(err => {
+      .catch((err) => {
         console.log(err);
         res.status(500).json(err);
       });
 
-
     // res.json(response);
   } else {
     //res.send("Error to add this note");
-    res.status(400).json({ message: "Something unexpected happened"})
+    res.status(400).json({ message: "Something unexpected happened" });
   }
 });
 
-app.delete('/api/notes/:id', (req, res) => {
+app.delete("/api/notes/:id", (req, res) => {
   console.log("Req Params: ", req.params);
   console.log("ID: ", req.params.id);
 
   // we need to read in our data
+  console.log(`${req.method} request received for notes`);
+  fs.readFile("./db/db.json", "utf8").then((notes) => {
+    console.log("Data: ", notes);
+    const originalNotes = JSON.parse(notes);
 
-  // we need to filter OUT the item with ID 
-      // for loop to loop through the array, 
-           // and id that does NOT match gets added to another array 
-           // if/else 
-           // forEach() - map() / filter()
+    const filteredNotes = originalNotes.filter((notes) => {
+      console.log("note id" + notes.id)
+      console.log("req.id" + req.params.id)
+      if (notes.id !== req.params.id) {
+        return true;
+      }
+    })
+    console.log(filteredNotes)
+
+    fs.writeFile("./db/db.json", JSON.stringify(filteredNotes))
+    .then(() => {
+      console.log("Note deleted successfully")
+      res.status(201).json({ message: "Note Deleted"})
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err)
+    });
+    
+  })
+  .catch((err) => {
+    console.log(err);
+    res.status(500).json(err)
+  });
+  
+
+ 
+
+  // const filteredNotes = originalNotes.filter((notes) => {
+  //   if (notes === req.params.id) {
+  //     return true;
+  //   }
+  // });
+
+  // console.log(filteredNotes);
+
+  // we need to filter OUT the item with ID
+  // for loop to loop through the array,
+  // and id that does NOT match gets added to another array
+  // if/else
+  // forEach() - map() / filter()
 
   // then we have to WRITE the NEW dataset to FILE
-})
+});
 
 app.listen(PORT, () =>
   console.log(`App listening at http://localhost:${PORT} 🚀`)
