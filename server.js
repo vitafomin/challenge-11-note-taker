42a4081fab14e48dd962457e975c5e7fbf35de96
Vita Fomin <vita.bukhalo@gmail.com>
2023-05-08 02:03:12 -0400

    cleaned up my coded, added comments

--- server.js
+++ server.js
@@ -1,9 +1,8 @@
+// Requiring/ importing in all the packages that are needed
 const express = require("express");
 const path = require("path");
 const fs = require("fs/promises");
-const { writeFile } = require("fs");
 const { uuid } = require("uuidv4");
-// const noteData = require
 
 const PORT = process.env.PORT || 3001;
 
@@ -22,11 +21,13 @@ app.get("/notes", (req, res) =>
   res.sendFile(path.join(__dirname, "/public/notes.html"))
 );
 
+// get method, to retrieve all the notes
 app.get("/api/notes", (req, res) => {
   console.log(`${req.method} request received for notes`);
   fs.readFile("./db/db.json").then((notes) => res.json(JSON.parse(notes)));
 });
 
+// post method to update/ add to the notes file (db.json)
 app.post("/api/notes", (req, res) => {
   console.log(`${req.method} request recieved to add a new note`);
 
@@ -42,7 +43,6 @@ app.post("/api/notes", (req, res) => {
     };
 
     console.log("New Note: ", newNote);
-    // });
 
     // Retrieving the Saved Data/Dataset
     fs.readFile("./db/db.json", "utf8")
@@ -67,81 +67,50 @@ app.post("/api/notes", (req, res) => {
         console.log(err);
         res.status(500).json(err);
       });
-
-    // res.json(response);
   } else {
-    //res.send("Error to add this note");
-    res.status(400).json({ message: "Something unexpected happened" });
+    res.status(400).json({ message: "Error to add this note" });
   }
 });
 
+// delete method to remove a note from the notes file
 app.delete("/api/notes/:id", (req, res) => {
   console.log("Req Params: ", req.params);
   console.log("ID: ", req.params.id);
 
   // we need to read in our data
   console.log(`${req.method} request received for notes`);
-  fs.readFile("./db/db.json", "utf8").then((notes) => {
-    console.log("Data: ", notes);
-    const originalNotes = JSON.parse(notes);
+  fs.readFile("./db/db.json", "utf8")
+    .then((notes) => {
+      console.log("Data: ", notes);
+      const originalNotes = JSON.parse(notes);
 
-    const filteredNotes = originalNotes.filter((notes) => {
-      console.log("note id" + notes.id)
-      console.log("req.id" + req.params.id)
-      if (notes.id !== req.params.id) {
-        return true;
-      }
-    })
-    console.log(filteredNotes)
+      // use the filter method to copy the array and create a new array
+      const filteredNotes = originalNotes.filter((notes) => {
+        console.log("note id" + notes.id);
+        console.log("req.id" + req.params.id);
+        if (notes.id !== req.params.id) {
+          return true;
+        }
+      });
+      console.log(filteredNotes);
 
-    fs.writeFile("./db/db.json", JSON.stringify(filteredNotes))
-    .then(() => {
-      console.log("Note deleted successfully")
-      res.status(201).json({ message: "Note Deleted"})
+      // write to the file the new array, this will overwrite file with the new array
+      fs.writeFile("./db/db.json", JSON.stringify(filteredNotes))
+        .then(() => {
+          console.log("Note deleted successfully");
+          res.status(201).json({ message: "Note Deleted" });
+        })
+        .catch((err) => {
+          console.log(err);
+          res.status(500).json(err);
+        });
     })
     .catch((err) => {
       console.log(err);
-      res.status(500).json(err)
+      res.status(500).json(err);
     });
-    
-  })
-  .catch((err) => {
-    console.log(err);
-    res.status(500).json(err)
-  });
-  
-
- 
-
-  // const filteredNotes = originalNotes.filter((notes) => {
-  //   if (notes === req.params.id) {
-  //     return true;
-  //   }
-  // });
-
-  // console.log(filteredNotes);
-
-  // we need to filter OUT the item with ID
-  // for loop to loop through the array,
-  // and id that does NOT match gets added to another array
-  // if/else
-  // forEach() - map() / filter()
-
-  // then we have to WRITE the NEW dataset to FILE
 });
 
 app.listen(PORT, () =>
   console.log(`App listening at http://localhost:${PORT} 🚀`)
 );
-// if (noteTitle && noteText) {
-//   const newNote = {
-//     noteTitle,
-//     noteText
-//   };
-
-//   readAndAppend(newNote, "./db/db.json");
-//   res.json("Added new note")
-// }
-// else {
-//   res.error("Error in adding a new note");
-// }
